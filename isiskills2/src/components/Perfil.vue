1441a6b4deaafb3c70ade7aa5330c9d7fcfa7231
rafaelp99 <frcp999@gmail.com>
2021-05-06 21:33:14 +0100

    Update Perfil.vue

--- isiskills2/src/components/Perfil.vue
+++ isiskills2/src/components/Perfil.vue
@@ -1,7 +1,37 @@
 <template>
     <div class="container">
         <h1>Meu Perfil</h1>
-    </div>
+    
+    <form @submit.prevent="submit">
+        
+            <label for="NIF">
+                NIF
+            </label>
+             <label for="Morada">
+                Morada
+            </label>
+             <label for="ZIP">
+                Código Postal
+            </label>
+             <label for="Cidade">
+                NIF
+            </label>
+             <label for="">
+                NIF
+            </label>
+             <label for="NIF">
+                NIF
+            </label>
+             <label for="NIF">
+                NIF
+            </label>
+             <label for="NIF">
+                NIF
+            </label>
+
+        
+    </form>
+    </div>  
 </template>
 <script>
 export default {
